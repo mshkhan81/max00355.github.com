13d5e9f290c636fb6b747c9fb8ea2b672b1b79bb
Frankie <max00355@gmail.com>
2013-05-27 00:42:33 -0400

    Fixed some stuff

--- blog.py
+++ blog.py
@@ -7,7 +7,7 @@ class Post:
     def main(self):
         title = raw_input("Title: ")
         month, day, year = time.localtime()[1], time.localtime()[2], time.localtime()[0]
-        data = "{0}/{1}/{2}".format(day, month, year)
+        data = "{0} / {1} / {2}".format(month, day, year)
         post = raw_input("Post: ")
         with open(self.filename,'r') as file:
             data = file.read().replace("<div class='posts'>", "<div class='posts'><p><span class='title'><h1>"+title+"</h1></span><span class='data'><h4>"+data+"</h4></span><span class='post'>"+post+"</span></p>")
